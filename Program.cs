8357c0e281a7117b3208f69815ac853c3591651e
salamkids <salam.kids.0811@gmail.com>
2022-06-18 10:28:13 +0100

    very good

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NewProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //Console.WriteLine("Hello World!");
+
+         
+
+
+           
+         
+        }
+    }
+}
